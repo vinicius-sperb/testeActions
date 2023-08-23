c8dc29379f81eeb7dba7cae4c8f3aa1a8caa1b75
kayqueh <kayque@brcore.com.br>
2023-08-23 08:41:05 -0300

    teste26

--- acessar_paginas.js
+++ acessar_paginas.js
@@ -1,10 +1,13 @@
-    const results = fetch('https://jsonplaceholder.typicode.com/todos/1')
+const fetch = require('node-fetch');
+
+    
+fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json))
+      .catch(error => console.error(error));
 
 
 
-console.log(results);
 
 
 
