414f4a30d59532df399060325a0b6c697a52c7d4
kayqueh <kayque@brcore.com.br>
2023-08-23 08:24:28 -0300

    teste2

--- acessar_paginas.js
+++ acessar_paginas.js
@@ -1,9 +1,3 @@
-import axios from "axios";
-
-axios.get('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
