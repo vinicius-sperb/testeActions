fbb8c87d01b0075858ee6ec3bf978bda964ea20e
kayqueh <kayque@brcore.com.br>
2023-08-24 08:32:20 -0300

    teste31

--- acessar_paginas.js
+++ acessar_paginas.js
@@ -1,5 +1,21 @@
+import { Octokit } from "octokit"
 
-    
-console.log('olá entrei aqui deu certo esta actions')
+const octokit = new Octokit({
+  auth: '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'
+});
 
+try {
+  const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+      owner: "octocat",
+      repo: "Spoon-Knife",
+    });
 
+    const titleAndAuthor = result.data.map(issue => ({
+        title: issue.title,
+        authorID: issue.user.id
+      }));
+  console.log(titleAndAuthor)
+
+} catch (error) {
+  console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+}
